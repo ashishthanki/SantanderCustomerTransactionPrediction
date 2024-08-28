68628a86e28285c2528b1432a0a3ee0a7c270062
Ashish Thanki <ashishthanki95@gmail.com>
2024-08-27 23:56:01 -0700

    Splitting data function added

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 from src.input import read_santander_data
-from src.transform import transform_data
+from src.transform import split_data, transform_data
 
 data_folder = Path(".") / "data"
 file_path = data_folder / "train.csv"
@@ -18,11 +18,13 @@ def main():
     "Main function to run Santander data science workflow."
     logger.info("Running Santander Data Science Workflow: %s", file_path)
     data = read_santander_data(file_path)
+    data = transform_data(data)
+    X, y = data.drop(columns=["target"]), data["target"]
+    X_train, y_train, X_test, y_test, X_val, y_val = split_data(X, y)
     logger.info(
         "Completed Running Santander Data Science Workflow, see outputs:"
     )
-    data = transform_data(data)
-    return data
+    return
 
 
 if __name__ == "__main__":
--- src/transform.py
+++ src/transform.py
@@ -1,53 +1,31 @@
 "Transformation functions."
 import logging
+from typing import Tuple
 
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 logger = logging.getLogger(__name__)
 
 
 def transform_data(data: pd.DataFrame) -> pd.DataFrame:
     logger.info("Transforming data")
-    data = change_dtypes(data)
-    data = remove_constant_columns(data)
+    data
     logger.info("Transformed data successfully.")
     return data
 
 
-def change_dtypes(data: pd.DataFrame) -> pd.DataFrame:
-    """Returns a DataFrame with changed column data types.
+def split_data(
+    X: pd.DataFrame, y: pd.Series
+) -> Tuple[np.array, np.array, np.array, np.array, np.array, np.array]:
+    logger.debug("Splitting data into train, test and validation sets.")
+    X_train, y_train, X_test_val, y_test_val = train_test_split(
+        X, y, random_state=42, test_size=0.4
+    )
+    X_test, y_test, X_val, y_val = train_test_split(
+        X_test_val, y_test_val, random_state=42, test_size=0.5
+    )
+    logger.debug("Split data into train, test and validation sets.")
 
-    Args:
-        data (pd.DataFrame): Data to perform.
-
-    Returns:
-        pd.DataFrame: Modified DataFrame.
-    """
-    logger.debug("Changing DataFrame dtypes")
-    for column_name in data.columns:
-        if ("date" in column_name) or ("time" in column_name):
-            logger.debug("Changing column name %s to datetime.", column_name)
-            data[column_name] = pd.to_datetime(data[column_name])
-        if "bytes" in column_name:
-            logger.debug("Changing column name %s to integer.", column_name)
-            data[column_name] = data[column_name].astype(int)
-        # convert to category if distinct values are less than 20
-        if data[column_name].nunique() < 25:
-            logger.debug("Changing column name %s to category.", column_name)
-            data[column_name] = data[column_name].astype("category")
-    return data
-
-
-def remove_constant_columns(data: pd.DataFrame) -> pd.DataFrame:
-    """Remove columns from the DataFrame that have constant values.
-
-    Args:
-        data (pd.DataFrame): Data to remove constant value columns.
-
-    Returns:
-        pd.DataFrame: Transformed DataFrame.
-    """
-    constant_columns = [col for col in data.columns if data[col].nunique() == 1]
-    logger.debug("Removing columns %s as they are constant.", constant_columns)
-    data = data.drop(columns=constant_columns)
-    return data
+    return (X_train, y_train, X_test, y_test, X_val, y_val)
