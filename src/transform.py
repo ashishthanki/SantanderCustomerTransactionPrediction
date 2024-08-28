34f86e7dd5e46e8421ead6ae1f30799c18ce8b81
Ashish Thanki <ashishthanki95@gmail.com>
2024-08-28 00:24:59 -0700

    Fix split bug and type hints

--- src/transform.py
+++ src/transform.py
@@ -2,7 +2,6 @@
 import logging
 from typing import Tuple
 
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -18,9 +17,11 @@ def transform_data(data: pd.DataFrame) -> pd.DataFrame:
 
 def split_data(
     X: pd.DataFrame, y: pd.Series
-) -> Tuple[np.array, np.array, np.array, np.array, np.array, np.array]:
+) -> Tuple[
+    pd.DataFrame, pd.Series, pd.DataFrame, pd.Series, pd.DataFrame, pd.Series
+]:
     logger.debug("Splitting data into train, test and validation sets.")
-    X_train, y_train, X_test_val, y_test_val = train_test_split(
+    X_train, X_test_val, y_train, y_test_val = train_test_split(
         X, y, random_state=42, test_size=0.4
     )
     X_test, y_test, X_val, y_val = train_test_split(
